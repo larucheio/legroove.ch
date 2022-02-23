7052f3f9f342f4a70822d8a59ccd0c39233b2f8b
rspt <julien@rspt.ch>
2022-02-23 10:15:28 +0100

    Fix datein agenda'scard

--- loop-templates/cards/agenda.php
+++ loop-templates/cards/agenda.php
@@ -18,7 +18,7 @@ defined( 'ABSPATH' ) || exit;
         <h2 class="h3"><?php normalize_headers(the_title()); ?></h2>
 
         <div class="d-flex justify-content-between">
-            <span class="small"><?php echo get_the_date('d.m.Y') ?> · AGENDA</span>
+            <span class="small"><?php echo tribe_get_start_date(null, null, 'd.m.Y') ?> · AGENDA</span>
             <a href="<?php echo get_permalink(); ?>" class="stretched-link text-reset text-decoration-none">+</a>
         </div>
     </div>
