bd351053da660813db5d2195f100b491d5f64943
rspt <julien@rspt.ch>
2022-03-10 13:37:27 +0100

    Month view

--- tribe/events/v2/month.php
+++ tribe/events/v2/month.php
@@ -25,66 +25,141 @@ $header_classes = [ 'tribe-events-header' ];
 if ( empty( $disable_event_search ) ) {
 	$header_classes[] = 'tribe-events-header--has-event-search';
 }
+
+$default_date        = date('Y-m');
+$selected_date_value = $this->get( [ 'bar', 'date' ] );
+if (!$selected_date_value) {
+	$selected_date_value = $default_date;
+}
+
+$tax_query = null;
+$path = array_filter(explode('/', parse_url( $_SERVER[ 'REQUEST_URI' ], PHP_URL_PATH )));
+$category = count($path) === 4 ? $path[3] : null;
+if ($category) {
+	$tax_query = array(
+		array(
+		'taxonomy' => 'tribe_events_cat',
+		'field' => 'slug',
+		'terms' => $category,
+		)
+	);
+}
+
+$start = date('Y-m-01', strtotime($selected_date_value));
+$end = date('Y-m-t', strtotime($selected_date_value));
+$groove_events = tribe_get_events([
+	'start_date' => $start,
+	'end_date' => $end,
+	'tax_query'=> $tax_query,
+]);
+
+$years = [];
+for ($i = intval(date('Y')); $i >= 2021; $i--) {
+	array_push($years, $i);
+}
+
+$categories = get_terms(
+	'tribe_events_cat',
+	array(
+    	'hide_empty' => false,
+	)
+);
 ?>
-<div
-	<?php tribe_classes( $container_classes ); ?>
-	data-js="tribe-events-view"
-	data-view-rest-nonce="<?php echo esc_attr( $rest_nonce ); ?>"
-	data-view-rest-url="<?php echo esc_url( $rest_url ); ?>"
-	data-view-rest-method="<?php echo esc_attr( $rest_method ); ?>"
-	data-view-manage-url="<?php echo esc_attr( $should_manage_url ); ?>"
-	<?php foreach ( $container_data as $key => $value ) : ?>
-		data-view-<?php echo esc_attr( $key ) ?>="<?php echo esc_attr( $value ) ?>"
-	<?php endforeach; ?>
-	<?php if ( ! empty( $breakpoint_pointer ) ) : ?>
-		data-view-breakpoint-pointer="<?php echo esc_attr( $breakpoint_pointer ); ?>"
-	<?php endif; ?>
->
-	<div class="tribe-common-l-container tribe-events-l-container">
-		<?php $this->template( 'components/loader', [ 'text' => __( 'Loading...', 'the-events-calendar' ) ] ); ?>
 
-		<?php $this->template( 'components/json-ld-data' ); ?>
+<div class="wrapper">
+	<div class="container">
+		<main class="site-main pb-5" id="main">
+			<header class="page-header">
+				<h1 class="page-title text-secondary display-1">AGENDA</h1>
+			</header>
 
-		<?php $this->template( 'components/data' ); ?>
+			<div class="row g-3">
+				<div class="col-md-3">
+					<div class="display-4 text-primary groove-font-family-headings">MOIS</div>
+					<select id="groove_calendar_filter_month" class="form-select form-select--groove">
+						<option value="01">Janvier</option>
+						<option value="02">Février</option>
+						<option value="03">Mars</option>
+						<option value="04">Avril</option>
+						<option value="05">Mai</option>
+						<option value="06">Juin</option>
+						<option value="07">Juillet</option>
+						<option value="08">Aout</option>
+						<option value="09">Septembre</option>
+						<option value="10">Octobre</option>
+						<option value="11">Novembre</option>
+						<option value="12">Décembre</option>
+					</select>
+				</div>
 
-		<?php $this->template( 'components/before' ); ?>
+				<div class="col-md-3">
+					<div class="display-4 text-primary groove-font-family-headings">ANNEE</div>
+					<select id="groove_calendar_filter_year" class="form-select form-select--groove">
+						<?php foreach ($years as $year): ?>
+							<option value="<?php echo $year ?>"><?php echo $year ?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
 
-		<header <?php tribe_classes( $header_classes ); ?>>
-			<?php $this->template( 'components/messages' ); ?>
+				<div class="col-md-3">
+					<div class="display-4 text-primary groove-font-family-headings">CATEGORIE</div>
+					<select id="groove_calendar_filter_category" class="form-select form-select--groove">
+						<option value="">-</option>
+						<?php foreach ($categories as $category): ?>
+							<option value="<?php echo $category->slug ?>"><?php echo $category->name ?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+			</div>
 
-			<?php $this->template( 'components/breadcrumbs' ); ?>
-
-			<?php $this->template( 'components/events-bar' ); ?>
-
-			<?php $this->template( 'month/top-bar' ); ?>
-		</header>
-
-		<?php $this->template( 'components/filter-bar' ); ?>
-
-		<div
-			class="tribe-events-calendar-month"
-			role="grid"
-			aria-labelledby="tribe-events-calendar-header"
-			aria-readonly="true"
-			data-js="tribe-events-month-grid"
-		>
-
-			<?php $this->template( 'month/calendar-header' ); ?>
-
-			<?php $this->template( 'month/calendar-body' ); ?>
-
-		</div>
-
-		<?php $this->template( 'components/messages', [ 'classes' => [ 'tribe-events-header__messages--mobile' ] ] ); ?>
-
-		<?php $this->template( 'month/mobile-events' ); ?>
-
-		<?php $this->template( 'components/ical-link' ); ?>
-
-		<?php $this->template( 'components/after' ); ?>
-
+			<div class="mt-4 row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+			<?php
+				foreach ($groove_events as $event) {
+					$this->setup_postdata( $event );
+					get_template_part( 'loop-templates/content', 'home' );
+					}
+			?>
+			</div>
+		</main>
 	</div>
-
 </div>
 
-<?php $this->template( 'components/breakpoints' ); ?>
+<script type="text/javascript">
+	let currentUrlPath = new URL(window.location.href).pathname
+	let currentUrlPathSplit = currentUrlPath.split("/").slice(2, -1)
+
+	let year = document.getElementById('groove_calendar_filter_year')
+	let month = document.getElementById('groove_calendar_filter_month')
+	let category = document.getElementById('groove_calendar_filter_category')
+
+	//Set the category value if present
+	if (currentUrlPathSplit.length > 1) {
+		category.value = currentUrlPathSplit[1]
+	}
+
+	let date = currentUrlPathSplit.pop()
+	if (date) {
+		year.value = date.slice(0, 4)
+		month.value = date.slice(5, 7)
+	} else {
+		let today = new Date()
+		year.value = today.getFullYear()
+		month.value = String(today.getMonth() + 1).padStart(2, '0')
+	}
+
+	year.onchange = updateFilter
+	month.onchange = updateFilter
+	category.onchange = updateFilter
+
+	function updateFilter() {
+		let url = "/agenda/"
+
+		if (category.value) {
+			url += `category/${category.value}/`
+		}
+
+		url += `${year.value}-${month.value}`
+
+		window.location.href = url
+	}
+</script>
