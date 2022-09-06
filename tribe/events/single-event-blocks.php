717770ac33f9607efbd425bdab4d0544bfe835fe
rspt <julien@rspt.ch>
2022-09-06 23:14:33 +0200

    Update date format

--- tribe/events/single-event-blocks.php
+++ tribe/events/single-event-blocks.php
@@ -19,7 +19,7 @@
 <div class="row">
 	<div class="col-12 col-lg-6 offset-lg-6">
 		<div class="text-secondary text-uppercase">
-			<?php echo tribe_get_start_date(null, false, 'd M y'); ?>
+			<?php echo tribe_get_start_date(null, false, 'l d F y'); ?>
 		</div>
 		<?php $this->template( 'single-event/title' ); ?>
 	</div>
