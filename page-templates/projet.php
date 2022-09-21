a2f4c8dc67aaae22cca1b08f4b419974971e7f18
rspt <julien@rspt.ch>
2022-09-21 15:26:26 +0200

    Vertical wave on project page

---
+++ page-templates/projet.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Template Name: Projet
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+
+<div class="wrapper" id="full-width-page-wrapper">
+
+	<div class="container" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area" id="primary">
+
+				<main class="site-main" id="main" role="main">
+
+					<?php
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'loop-templates/content', 'page' );
+					}
+					?>
+
+				</main><!-- #main -->
+
+			</div><!-- #primary -->
+
+		</div><!-- .row end -->
+
+	</div><!-- #content -->
+
+</div><!-- #full-width-page-wrapper -->
+
+
+<style media="screen">
+	.groove-bg-wave {
+		display: none !important;
+	}
+
+	/* .cls-1 {
+		stroke: yellow !important;
+		fill: yellow !important;
+	} */
+
+	.site.container {
+		background-image: url('<?php echo get_template_directory_uri() . "/images/wave-projet.svg" ?>');
+		background-repeat: no-repeat;
+		background-position: center;
+	}
+</style>
+
+<?php
+get_footer();
