ff12399e6836206d5e4f0df46a27d31ddbfff903
rspt <julien@rspt.ch>
2024-06-04 14:47:44 +0200

    Update home.php

--- home.php
+++ home.php
@@ -29,6 +29,20 @@ $the_query = new WP_Query( array(
 	  'order' => 'ASC',
 	  'posts_per_page' => '3',
 ));
+
+// News
+$category_id = 5;
+$news = new WP_Query(array(
+    'cat'            => $category_id,
+    'posts_per_page' => 3
+));
+
+// Media
+$category_id = 4;
+$media = new WP_Query(array(
+    'cat'            => $category_id,
+    'posts_per_page' => 3
+));
 ?>
 
 <div class="wrapper pt-0" id="index-wrapper">
@@ -49,6 +63,30 @@ $the_query = new WP_Query( array(
 						// Start the Loop.
 						while ( $the_query->have_posts() ) {
 							$the_query->the_post();
+							get_template_part( 'loop-templates/content', 'home' );
+						}
+					}
+					?>
+				</div>
+				
+				<div class="mt-5 row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+					<?php
+					if ( $news->have_posts() ) {
+						// Start the Loop.
+						while ( $news->have_posts() ) {
+							$news->the_post();
+							get_template_part( 'loop-templates/content', 'home' );
+						}
+					}
+					?>
+				</div>
+				
+				<div class="mt-5 row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+					<?php
+					if ( $media->have_posts() ) {
+						// Start the Loop.
+						while ( $media->have_posts() ) {
+							$media->the_post();
 							get_template_part( 'loop-templates/content', 'home' );
 						}
 					}
